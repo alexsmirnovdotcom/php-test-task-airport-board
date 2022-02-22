6b8eb1f44e156c42c3a09c93f0a70e9946679235
Alexey <me@alexsmirnov.com>
2022-02-22 11:29:35 +0500

    Рефакторинг FlightRepository для передачи DateTime во Flight вместо строк со временем
    Плюс косметические изменения

--- src/Repository/FlightRepository.php
+++ src/Repository/FlightRepository.php
@@ -11,6 +11,7 @@ class FlightRepository extends AbstractRepository
     private const TO_DATA_KEY = 'to';
     private const AIRPORT_DATA_KEY = 'airport';
     private const TIME_DATA_KEY = 'time';
+    private const DATE_DATA_KEY = 'date';
 
     private JsonParser $jsonParser;
     private AirportRepository $airportRepository;
@@ -29,7 +30,7 @@ class FlightRepository extends AbstractRepository
     protected function loadData(): array
     {
         return array_map(
-            function(array $flightData): Flight {
+            function (array $flightData): Flight {
                 return $this->buildFlight($flightData);
             },
             $this->jsonParser->load('flight.json')
@@ -38,17 +39,54 @@ class FlightRepository extends AbstractRepository
 
     private function buildFlight(mixed $flightData): Flight
     {
-        $this->assertKeysExists($flightData, [self::FROM_DATA_KEY, self::TO_DATA_KEY]);
+        $this->assertKeysExists($flightData, [
+            self::FROM_DATA_KEY,
+            self::TO_DATA_KEY
+        ]);
+
         $fromData = $flightData[self::FROM_DATA_KEY];
         $toData = $flightData[self::TO_DATA_KEY];
-        $this->assertKeysExists($fromData, [self::AIRPORT_DATA_KEY, self::TIME_DATA_KEY]);
-        $this->assertKeysExists($toData, [self::AIRPORT_DATA_KEY, self::TIME_DATA_KEY]);
+
+        $this->assertKeysExists($fromData,
+            [
+                self::AIRPORT_DATA_KEY,
+                self::TIME_DATA_KEY,
+                self::DATE_DATA_KEY
+            ]
+        );
+
+        $this->assertKeysExists($toData,
+            [
+                self::AIRPORT_DATA_KEY,
+                self::TIME_DATA_KEY,
+                self::DATE_DATA_KEY
+            ]
+        );
+
+        $fromAirport = $this->airportRepository->getAirport($fromData[self::AIRPORT_DATA_KEY]);
+        $toAirport = $this->airportRepository->getAirport($toData[self::AIRPORT_DATA_KEY]);
+
+        $fromDateTime = $this->buildDateTime(
+            $fromData[self::DATE_DATA_KEY],
+            $fromData[self::TIME_DATA_KEY],
+            $fromAirport->getTimezone()
+        );
+        $toDateTime = $this->buildDateTime(
+            $toData[self::DATE_DATA_KEY],
+            $toData[self::TIME_DATA_KEY],
+            $toAirport->getTimezone()
+        );
 
         return new Flight(
-            $this->airportRepository->getAirport($fromData[self::AIRPORT_DATA_KEY]),
-            $fromData[self::TIME_DATA_KEY],
-            $this->airportRepository->getAirport($toData[self::AIRPORT_DATA_KEY]),
-            $toData[self::TIME_DATA_KEY]
+            $fromAirport,
+            $fromDateTime,
+            $toAirport,
+            $toDateTime,
         );
+    }
+
+    private function buildDateTime(string $date, string $time, string $timezone): \DateTime
+    {
+        return new \DateTime("{$date} {$time}", new \DateTimeZone($timezone));
     }
 }
