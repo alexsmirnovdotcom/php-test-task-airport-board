14cd5331a6060a03c1df14fb9665e447b6475180
Alexey <me@alexsmirnov.com>
2022-02-22 10:55:11 +0500

    Добавлен парсинг timezone в AirportRepository для Airport

--- src/Repository/AirportRepository.php
+++ src/Repository/AirportRepository.php
@@ -10,6 +10,7 @@ class AirportRepository extends AbstractRepository
     private const CODE_DATA_KEY = 'code';
     private const NAME_DATA_KEY = 'name';
     private const CITY_DATA_KEY = 'city';
+    private const TIMEZONE_DATA_KEY = 'timeZone';
 
     private JsonParser $jsonParser;
 
@@ -45,7 +46,8 @@ class AirportRepository extends AbstractRepository
         return new Airport(
             $airportData[self::CODE_DATA_KEY],
             $airportData[self::NAME_DATA_KEY],
-            $airportData[self::CITY_DATA_KEY]
+            $airportData[self::CITY_DATA_KEY],
+            $airportData[self::TIMEZONE_DATA_KEY],
         );
     }
 
